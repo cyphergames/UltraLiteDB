8e63173386d3882bea6290e880410b8d9188950b
Jeremy Friesen <rejemy@yahoo.com>
2020-03-02 16:33:58 -0800

    Ported over some tests from LiteDB 4.0

---
+++ UltraLiteDB.Tests/Database/DateTimeMinMax_Tests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UltraLiteDB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UltraLiteDB.Tests.Database
+{
+    #region Model
+
+    public class DateTimeTest
+    {
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
+    }
+
+    #endregion
+
+    [TestClass]
+    public class DateTimeMinMax_Tests
+    {
+        [TestMethod]
+        public void DateTimeMinMax_Test()
+        {
+            var memory = new MemoryStream();
+
+            using (var db = new UltraLiteDatabase(memory))
+            {
+                var col = db.GetCollection<DateTimeTest>();
+                col.EnsureIndex("Date");
+
+                col.Insert(new DateTimeTest() { Id = 1, Date = new DateTime(2018, 02, 22, 0, 0, 0) });
+                col.Insert(new DateTimeTest() { Id = 2, Date = new DateTime(2018, 02, 22, 23, 59, 59) });
+
+                MinMaxCommon(col);
+            }
+
+            using (var db = new UltraLiteDatabase(memory))
+            {
+                var col = db.GetCollection<DateTimeTest>();
+
+                MinMaxCommon(col);
+
+                col.Insert(new DateTimeTest() { Id = 3, Date = new DateTime(2018, 02, 21, 23, 59, 59) });
+                col.Insert(new DateTimeTest() { Id = 4, Date = new DateTime(2018, 02, 23, 0, 0, 0) });
+                col.Insert(new DateTimeTest() { Id = 5, Date = new DateTime(2018, 02, 22, 0, 0, 1) });
+                col.Insert(new DateTimeTest() { Id = 6, Date = new DateTime(2018, 02, 22, 23, 59, 58) });
+
+                MinMaxCommon(col);
+            }
+
+            using (var db = new UltraLiteDatabase(memory))
+            {
+                var col = db.GetCollection<DateTimeTest>();
+
+                MinMaxCommon(col);
+            }
+        }
+
+        private void MinMaxCommon(UltraLiteCollection<DateTimeTest> coll)
+        {
+            var searchdatetime = new DateTime(2018, 02, 22, 0, 0, 10);
+
+            var min = coll.Min("Date").AsDateTime;
+            var max = coll.Max("Date").AsDateTime;
+
+            var smaller = coll.FindOne(Query.LT("Date", searchdatetime));
+            var greater = coll.FindOne(Query.GT("Date", searchdatetime));
+
+            var all = coll.FindAll().ToList();
+
+            var linqmin = all.Min(x => x.Date);
+            var linqmax = all.Max(x => x.Date);
+
+            Assert.AreEqual(min, linqmin);
+            Assert.AreEqual(max, linqmax);
+        }
+    }
+}
---
+++ UltraLiteDB.Tests/Mapper/ULongList_Tests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace UltraLiteDB.Tests.Mapper
+{
+    #region Model
+
+    public class Gang
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public ulong LeaderId { get; set; }
+        public ulong GuildId { get; set; }
+        public List<ulong> Members { get; set; }
+        public double Wealth { get; set; } = 0.0;
+        public DateTime Raid { get; set; } = DateTime.UtcNow.AddYears(-1);
+    }
+
+    #endregion
+
+    [TestClass]
+    public class ULongList_Tests
+    {
+        [TestMethod]
+        public void ULongList_Mapper()
+        {
+            using (var file = new TempFile())
+            using (var db = new UltraLiteDatabase(file.Filename))
+            {
+                var col = db.GetCollection<Gang>();
+
+                col.Insert(new Gang
+                {
+                    GuildId = 1,
+                    LeaderId = 2,
+                    Members = new List<ulong> { 5, 6, 7}
+                });
+
+                ulong userId = 5;
+                ulong guildId = 1;
+
+                Query q = Query.And(Query.EQ("GuildId", guildId), Query.EQ("LeaderId", userId));
+                var e = col.Exists(q);
+
+                //Assert.IsTrue(e);
+
+            }
+        }
+    }
+}
